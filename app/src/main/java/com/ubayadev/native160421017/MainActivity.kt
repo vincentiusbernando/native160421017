9cbe3514fdf0a54fadd79e3925044dd41c14f410
palkon-code <120707573+palkon-code@users.noreply.github.com>
2024-02-22 14:30:59 +0700

    Update Main Activity

--- app/src/main/java/com/ubayadev/native160421017/MainActivity.kt
+++ app/src/main/java/com/ubayadev/native160421017/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var nrp="160421017"
+    var nama="Vincentius Bernando Wijaya"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

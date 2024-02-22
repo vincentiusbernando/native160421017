66ad73f9456bda0067264cf4e645025bc1d45433
Andr3B0t <andreasjcool281@gmail.com>
2024-02-22 14:38:58 +0700

    tambahan nrp nama

--- app/src/main/java/com/ubayadev/native160421017/MainActivity.kt
+++ app/src/main/java/com/ubayadev/native160421017/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var nrp = "160421013"
+    var nama = "Andreas Jaya Kusuma"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
